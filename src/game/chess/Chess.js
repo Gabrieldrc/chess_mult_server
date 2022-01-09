f030f4fafc51dfbc5f6002cb4d35ec1e35aace21
Gabrieldrc <gabriel.drc25@gmail.com>
2022-01-08 21:04:49 -0300

    TDD-0004 Chess tdd

--- src/game/chess/Chess.js
+++ src/game/chess/Chess.js
@@ -11,56 +11,47 @@ module.exports = class Chess {
             for (let j = 0; j < boardLength; j++) {
                 if (i == 0) {
                     if (j == 0 || j == boardLength - 1) {
-                        this.board[i][j] = PieceFactory.getPiece('ROOK', 2, i, j);
-                    }
-                    if (j == 1 || j == boardLength - 2) {
+                        this.board[i].push(PieceFactory.getPiece('ROOK', 2, i, j));
+                    } else if (j == 1 || j == boardLength - 2) {
                         // Caballo
-                        this.board[i][j] = PieceFactory.getPiece('KNIGHT', 2, i, j);
-                    }
-                    if (j == 2 || j == boardLength - 3) {
+                        this.board[i].push(PieceFactory.getPiece('KNIGHT', 2, i, j));
+                    } else if (j == 2 || j == boardLength - 3) {
                         // Alfil
-                        this.board[i][j] = PieceFactory.getPiece('BISHOP', 2, i, j);
-                    }
-                    if (j == 3) {
+                        this.board[i].push(PieceFactory.getPiece('BISHOP', 2, i, j));
+                    } else if (j == 3) {
                         // Rey
-                        this.board[i][j] = PieceFactory.getPiece('KING', 2, i, j);
-                    }
-                    if (j == 4) {
+                        this.board[i].push(PieceFactory.getPiece('KING', 2, i, j));
+                    } else if (j == 4) {
                         // Reina
-                        this.board[i][j] = PieceFactory.getPiece('QUEEN', 2, i, j);
-                    }
-                    else {
-                        this.board[i][j] = PieceFactory.getPiece();
+                        this.board[i].push(PieceFactory.getPiece('QUEEN', 2, i, j));
+                    } else {
+                        this.board[i].push(PieceFactory.getPiece());
                     }
                 } else if (i == 1) {
-                    this.board[i][j] = new Pawn(2, i, j);
+                    this.board[i].push(PieceFactory.getPiece('PAWN',2, i, j));
                 } else if (i == boardLength - 2) {
-                    this.board[i][j] = new Pawn(1, i, j);
+                    this.board[i].push(PieceFactory.getPiece('PAWN',1, i, j));
                 } else if (i == boardLength - 1) {
                     if (j == 0 || j == boardLength - 1) {
-                        this.board[i][j] = PieceFactory.getPiece('ROOK', 1, i, j);
-                    }
-                    if (j == 1 || j == boardLength - 2) {
+                        this.board[i].push(PieceFactory.getPiece('ROOK', 1, i, j));
+                    } else if (j == 1 || j == boardLength - 2) {
                         // Caballo
-                        this.board[i][j] = PieceFactory.getPiece('KNIGHT', 2, i, j);
-                    }
-                    if (j == 2 || j == boardLength - 3) {
+                        this.board[i].push(PieceFactory.getPiece('KNIGHT', 1, i, j));
+                    } else if (j == 2 || j == boardLength - 3) {
                         // Alfil
-                        this.board[i][j] = PieceFactory.getPiece('BISHOP', 2, i, j);
-                    }
-                    if (j == 3) {
+                        this.board[i].push(PieceFactory.getPiece('BISHOP', 1, i, j));
+                    } else if (j == 3) {
                         // Rey
-                        this.board[i][j] = PieceFactory.getPiece('KING', 2, i, j);
-                    }
-                    if (j == 4) {
+                        this.board[i].push(PieceFactory.getPiece('KING', 1, i, j));
+                    } else if (j == 4) {
                         // Reina
-                        this.board[i][j] = PieceFactory.getPiece('QUEEN', 2, i, j);
+                        this.board[i].push(PieceFactory.getPiece('QUEEN', 1, i, j));
                     }
                     else {
-                        this.board[i][j] = PieceFactory.getPiece();
+                        this.board[i].push(PieceFactory.getPiece());
                     }
                 } else {
-                    this.board[i][j] = PieceFactory.getPiece();
+                    this.board[i].push(PieceFactory.getPiece());
                 }
             }
         }
@@ -72,9 +63,9 @@ module.exports = class Chess {
             this.board[x2][y2] = this.board[x1][y1]
             this.board[x1][y1] = PieceFactory.getPiece();
 
-            return this.board
+            return true
         }
-        return
+        return false
     }
 
     getBoardData() {
---
+++ test/chessGame/Chess.test.js
@@ -0,0 +1,69 @@
+const Chess = require('../../src/game/chess/Chess')
+const ChessPieceAbstract = require('../../src/game/chess/ChessPieceAbstract')
+
+let underProve, board
+describe('Chess behavior', ()=>{
+    beforeAll(()=>{
+        underProve = new Chess()
+        underProve.newGame()
+        board = underProve.board
+    })
+    describe('newGame should', ()=>{
+        test(' return a array 8 x 8', ()=>{
+            expect(board instanceof Array).toBeTruthy()
+            expect(board.length).toStrictEqual(8)
+            for (let i = 0; i < board.length; i++) {
+                expect(board[i].length).toStrictEqual(8)
+            }
+        })
+        test(' return the board in a particular order', ()=>{
+            const boardExpected = [
+                ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook'],
+                ['pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn'],
+                ['', '', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', '', ''],
+                ['pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn'],
+                ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook'],
+            ]
+            let boardRecieve = []
+            for (let i = 0; i < board.length; i++) {
+                boardRecieve.push([])
+                for (let j = 0; j < board[i].length; j++) {
+                    boardRecieve[i].push(board[i][j].name)
+                }
+            }
+            expect(boardRecieve).toStrictEqual(boardExpected)
+        })
+        test(' return a new board where half pieces are from player 2 and the second one from player 1', ()=>{
+            for (let i = 0; i < board.length; i++) {
+                for (let j = 0; j < board[i].length; j++) {
+                    if (i <= 1) {
+                        expect(board[i][j].player).toStrictEqual(2)
+                    } else if (i >= board.length - 2) {
+                        expect(board[i][j].player).toStrictEqual(1)
+                    } else {
+                        expect(board[i][j].player).toStrictEqual(0)
+                    }
+                }
+            }
+        })
+    })
+    describe('move should', ()=>{
+        beforeEach(() => {
+            underProve.newGame()
+        })
+        test(' return true if can move', ()=>{
+            expect(underProve.move(1, 0, 2, 0)).toBeTruthy()
+        })
+        test(' leave an emptyspace where the piece was and placed at the new places', ()=>{
+            underProve.move(1, 0, 2, 0)
+            expect(underProve.board[1][0].name).toStrictEqual('')
+            expect(underProve.board[2][0].name).toStrictEqual('pawn')
+        })
+        test(' return false if can not move', ()=>{
+            expect(underProve.move(0, 0, 5, 0)).toBeFalsy()
+        })
+    })
+})
